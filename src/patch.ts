a41f038ac0b3075edfc05d4e39e9c1752ffed86f
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2018-01-09 00:45:49 +0100

    fix(patch) fixed casing in method names

--- src/patch.ts
+++ src/patch.ts
@@ -11,27 +11,27 @@ export interface BuiltPath {
 export class Patch<T> {
     operations: Operation[] = [];
 
-    Add(path: Path<T>, value: any): Patch<T> {
+    add(path: Path<T>, value: any): Patch<T> {
         this.operations.push({ op: 'add', path: resolvePath(path), value: value });
         return this;
     }
 
-    Remove(path: Path<T>): Patch<T> {
+    remove(path: Path<T>): Patch<T> {
         this.operations.push({ op: 'remove', path: resolvePath(path) });
         return this;
     }
 
-    Replace(path: Path<T>, value: any): Patch<T> {
+    replace(path: Path<T>, value: any): Patch<T> {
         this.operations.push({ op: 'replace', path: resolvePath(path), value: value });
         return this;
     }
 
-    Copy(from: Path<T>, path: Path<T>): Patch<T> {
+    copy(from: Path<T>, path: Path<T>): Patch<T> {
         this.operations.push({ op: 'copy', from: resolvePath(from), path: resolvePath(path) });
         return this;
     }
 
-    Move(from: Path<T>, path: Path<T>): Patch<T> {
+    move(from: Path<T>, path: Path<T>): Patch<T> {
         this.operations.push({ op: 'move', from: resolvePath(from), path: resolvePath(path) });
         return this;
     }
