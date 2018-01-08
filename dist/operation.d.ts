99a5d5db073a3d7c72953e5959b23d5af20e10a6
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2018-01-08 22:07:19 +0100

    feat(all) Initial commit

---
+++ dist/operation.d.ts
@@ -0,0 +1,30 @@
+export interface AddOperation {
+    op: 'add';
+    path: string;
+    value: any;
+}
+export interface RemoveOperation {
+    op: 'remove';
+    path: string;
+}
+export interface ReplaceOperation {
+    op: 'replace';
+    path: string;
+    value: any;
+}
+export interface CopyOperation {
+    op: 'copy';
+    from: string;
+    path: string;
+}
+export interface MoveOperation {
+    op: 'move';
+    from: string;
+    path: string;
+}
+export interface TestOperation {
+    op: 'test';
+    path: string;
+    value: string;
+}
+export declare type Operation = AddOperation | RemoveOperation | ReplaceOperation | CopyOperation | MoveOperation | TestOperation;
